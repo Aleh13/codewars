1fdc4eadf0f495f74237d03093f7af47fa282816
Aleh13 <q4wk13@gmail.com>
2021-07-29 08:45:09 +0300

    Added kyu ruby

---
+++ ruby/8_kyu_returning strings.rb
@@ -0,0 +1,12 @@
+=begin
+
+Make a function that will return a greeting statement that uses an input;
+your program should return, "Hello, <name> how are you doing today?".
+
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/ruby
+
+=end
+
+def greet(name)
+  "Hello, #{name} how are you doing today?"
+end
