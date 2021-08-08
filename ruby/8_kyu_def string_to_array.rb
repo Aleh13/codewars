f1ce104567302c2c3cc882073e8fc732d572c272
Aleh13 <q4wk13@gmail.com>
2021-08-08 11:27:16 +0300

    Added kyu ruby

---
+++ ruby/8_kyu_def string_to_array.rb
@@ -0,0 +1,11 @@
+=begin
+
+Write a function to split a string and convert it into an array of words.
+
+https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/ruby
+
+=end
+
+def string_to_array(string)
+  string.split
+end
